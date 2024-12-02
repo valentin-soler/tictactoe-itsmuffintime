6765daa9bb06db0af707c1f102b4222b60898ae2
VoXaN24 <30030208+VoXaN24@users.noreply.github.com>
2024-12-02 11:17:26 +0100

    Victoire Cas 1 ([0][0])

--- verif_victoire.py
+++ verif_victoire.py
@@ -2,3 +2,16 @@ def nul(grille):
     if " " in grille:
         return False
     return True
+
+def victoire(grille,pos,signe): #Pos --> Position du coup joué # signe --> X ou O
+    #Cas 1 pos 1 -> [0][0]
+    if pos == 1: 
+        if grille[0][1] == signe and grille[0][2] == signe:
+            return True
+        elif grille[1][1] == signe and grille[2][2] == signe:
+            return True
+        elif grille[1][0] == signe and grille[1][0] == signe:
+            return True
+        else :
+            return False
+    #Cas 2 pos 2 -> [0][1]
